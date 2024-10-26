c79d4140e03724499ccfec378212812f609ccfbb
AlenAntler <97449518+AlenAntler@users.noreply.github.com>
2024-10-26 23:18:11 +0200

    Add fishing action READ Desc
    Added an action so we have some more diversity.
    
    It would be nice if we added that while trees are burning they can't request plan for chopping wood, and then more of them go fishing. Vice versa when the chaos event in fishing happens (can be just a kraken spawning instead of burning tree or so).
    
    Needs to be fixed:
    Both chopping wood and fishing they get stuck after a while there, they dont request new plans.
    Also damage isnt taken from the chaos events anymore.

---
+++ Scenes/Chaos/fishing_spot.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var sprite = $Sprite2D
+@onready var clickable = $Clickable
+@onready var chaos = $ChaosTriggerable
+@onready var damage_area = $DamageArea
+
+func _ready():
+	clickable.set_process(false)
+	ActionPointsManager.register_action_point(position)
+
+func _on_node_trigger_chaos() -> void:
+	sprite.self_modulate = Color.RED
+	clickable.clicking_is_enabled = true
+	damage_area.damage_area_active = true
+
+func _on_clickable_on_clicked() -> void:
+	clickable.clicking_is_enabled = false
+	damage_area.damage_area_active = false
+	chaos.deactivate_chaos()
+	sprite.self_modulate = Color.WHITE
