06d252c61aac9bbb8914443ea1e3917abdb876e4
arget4me <sn.jn@telia.com>
2024-10-26 23:31:09 +0200

    Update fire effect

--- Scripts/Chaos/fire_tree.gd
+++ Scripts/Chaos/fire_tree.gd
@@ -4,18 +4,24 @@ extends Node2D
 @onready var clickable = $Clickable
 @onready var chaos = $ChaosTriggerable
 @onready var damage_area = $DamageArea
+@onready var fire_node = $FireNode
+
+@export var fire_color := Color.WHITE 
 
 func _ready():
 	clickable.set_process(false)
 	ActionPointsManager.register_action_point(position)
+	fire_node.visible = false
 
 func _on_node_trigger_chaos() -> void:
-	sprite.self_modulate = Color.RED
+	sprite.self_modulate = fire_color
 	clickable.clicking_is_enabled = true
 	damage_area.damage_area_active = true
+	fire_node.visible = true
 
 func _on_clickable_on_clicked() -> void:
 	clickable.clicking_is_enabled = false
 	damage_area.damage_area_active = false
+	fire_node.visible = false
 	chaos.deactivate_chaos()
 	sprite.self_modulate = Color.WHITE
---
+++ art/fire/fire_anim.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+@onready var anim = $AnimationPlayer
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	anim.play("Burn")
