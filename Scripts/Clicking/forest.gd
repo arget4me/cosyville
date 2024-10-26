47ed26bedf8b5752219e55f1b67d120e615fe806
arget4me <sn.jn@telia.com>
2024-10-26 09:52:46 +0200

    Remove change cursor example - prepare for toolbar

--- Scripts/Clicking/forest.gd
+++
@@ -1,8 +0,0 @@
-extends Sprite2D
-
-func _on_area_2d_mouse_entered() -> void:
-	MouseImageManager.push_cursor(MouseImageManager.CURSOR_STATE.WATERBUCKET)
-
-
-func _on_area_2d_mouse_exited() -> void:
-	MouseImageManager.pop_cursor()
