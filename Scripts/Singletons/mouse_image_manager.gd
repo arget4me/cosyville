4df828a2892012cbf5a083bf9f7a747c3bf6f93f
arget4me <sn.jn@telia.com>
2024-10-26 15:29:50 +0200

    Cursor states and aspect and fullscreen

--- Scripts/Singletons/mouse_image_manager.gd
+++ Scripts/Singletons/mouse_image_manager.gd
@@ -1,10 +1,16 @@
 extends Node
 
-var pointer = load("res://art/PointingHand.png")
-var waterbucket = load("res://art/WaterBucket.png")
+var pointer = load("res://art/cursors/PointingHand.png")
+var waterbucket = load("res://art/cursors/WaterBucket.png")
+
+var pointer_hover = load("res://art/cursors/PointingHand_interactable.png")
+var waterbucket_hover = load("res://art/cursors/WaterBucket_interactable.png")
 
 enum CURSOR_STATE{POINTER, WATERBUCKET}
 var state_stack = []
+var current_state = CURSOR_STATE.POINTER
+
+var interactable : bool = false
 
 func _ready() -> void:
 	push_cursor(CURSOR_STATE.POINTER)
@@ -22,7 +28,12 @@ func pop_cursor():
 		set_cursor(state_stack[len(state_stack) - 1])
 
 func set_cursor(state : CURSOR_STATE):
-	if state == CURSOR_STATE.POINTER:
-		Input.set_custom_mouse_cursor(pointer)
-	if state == CURSOR_STATE.WATERBUCKET:
-		Input.set_custom_mouse_cursor(waterbucket)
+	current_state = state
+	set_interactable(interactable)
+
+func set_interactable(value : bool):
+	interactable = value
+	if current_state == CURSOR_STATE.POINTER:
+		Input.set_custom_mouse_cursor(pointer_hover if interactable else pointer)
+	if current_state == CURSOR_STATE.WATERBUCKET:
+		Input.set_custom_mouse_cursor(waterbucket_hover if interactable else waterbucket)
