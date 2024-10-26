a8272d77eacebb1a7908288cd44e43da7cd99648
arget4me <sn.jn@telia.com>
2024-10-26 13:44:25 +0200

    Fix ui in scene transition

--- Scripts/Singletons/clicking_manager.gd
+++ Scripts/Singletons/clicking_manager.gd
@@ -4,14 +4,23 @@ signal on_score_update(score : int)
 
 var score = 0
 var score_per_second = 100
+var timer : Timer
 
 func _ready():
-	var timer := Timer.new()
+	timer = Timer.new()
 	timer.wait_time = 1.0
 	timer.one_shot = false
 	timer.timeout.connect(add_score_per_second)
 	add_child(timer)
-	timer.start()
+	GameManager.on_game_state_changed.connect(_reset_score)
+
+func _reset_score(state : GameManager.game_state):
+	if state == GameManager.game_state.PLAYING:
+		timer.start()
+		score = 0
+		on_score_update.emit(score)
+	else:
+		timer.stop()
 
 func add_score_per_second():
 	add_score(score_per_second)
