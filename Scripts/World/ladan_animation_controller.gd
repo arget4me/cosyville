dbb736e4f7c9347e3e2c2a12a0b592e47b7e8cff
markah213 <markah213@studentmail.oru.se>
2024-10-27 11:19:47 +0100

    Added score animation to Ladan

--- Scripts/World/ladan_animation_controller.gd
+++ Scripts/World/ladan_animation_controller.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var animtree = $AnimationTree
+@onready var score_animation = preload("res://Scenes/UI/score_animation.tscn")
 var state_machine
 
 func _ready() -> void:
@@ -8,3 +9,6 @@ func _ready() -> void:
 
 func _on_clickable_on_display_clickable() -> void:
 	state_machine.travel("Click")
+	var new_score_animation = score_animation.instantiate()
+	new_score_animation.position.y -= 100
+	add_child(new_score_animation)
