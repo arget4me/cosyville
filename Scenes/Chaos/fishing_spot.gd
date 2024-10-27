2ef3de756c74bb7130eceec493c323ed84b88b94
karl97 <karl.97@live.se>
2024-10-27 14:50:26 +0100

    fishing spot visibility

--- Scenes/Chaos/fishing_spot.gd
+++ Scenes/Chaos/fishing_spot.gd
@@ -1,13 +1,4 @@
 extends Node2D
 
-@onready var sprite = $Sprite2D
-@onready var clickable = $Clickable
-
-
 func _ready():
-	clickable.set_process(false)
 	ActionPointsManager.register_action_point(position, ActionPointsManager.action_point_type.FISH)
-
-func _on_clickable_on_clicked() -> void:
-	clickable.clicking_is_enabled = false
-	sprite.self_modulate = Color.WHITE
