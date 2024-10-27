54612689313c55df64d9791f343ac32049ad4b40
karl97 <karl.97@live.se>
2024-10-27 14:35:46 +0100

    house turns in

--- Scripts/turn_in_house.gd
+++ Scripts/turn_in_house.gd
@@ -3,7 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	ActionPointsManager.register_action_point(global_position, ActionPointsManager.action_point_type.HOUSE)
+	var offset_position = global_position
+	offset_position.y += 10
+	ActionPointsManager.register_action_point(offset_position, ActionPointsManager.action_point_type.HOUSE)
 	pass # Replace with function body.
 
 
