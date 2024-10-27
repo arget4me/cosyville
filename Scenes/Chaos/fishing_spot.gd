1adbc2a57a21b7e8b3f4092a013a693c0a9d22bb
karl97 <karl.97@live.se>
2024-10-27 12:27:39 +0100

    added distinction between points

--- Scenes/Chaos/fishing_spot.gd
+++ Scenes/Chaos/fishing_spot.gd
@@ -6,7 +6,7 @@ extends Node2D
 
 func _ready():
 	clickable.set_process(false)
-	ActionPointsManager.register_action_point(position)
+	ActionPointsManager.register_action_point(position, ActionPointsManager.action_point_type.FISH)
 
 func _on_clickable_on_clicked() -> void:
 	clickable.clicking_is_enabled = false
